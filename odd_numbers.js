f1609de64857713bb38df92f4f6f3cc5573f7be4
KPrerana4 <prerana.kannegundla@thoughtworks.com>
2020-02-18 14:44:51 +0530

    list of odd numbers

---
+++ odd_numbers.js
@@ -0,0 +1,11 @@
+var numbersList = [1, 3, 5, 2, 4, 6, 7, 8, 99, 100];
+var number;
+var oddNumbers = [];
+for(number of numbersList)
+{
+    if(number % 2 != 0)
+    {
+        oddNumbers.push(number);
+    }
+}
+console.log(oddNumbers);
