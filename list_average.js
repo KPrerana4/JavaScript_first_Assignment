ff49088eba32fe19ac919fece93a23f42bcc748b
KPrerana4 <prerana.kannegundla@thoughtworks.com>
2020-02-19 09:14:19 +0530

    average of numbers in list

---
+++ list_average.js
@@ -0,0 +1,15 @@
+function average()
+{
+    let number, listLength = list.length, sum = 0;
+    for(number of list)
+    {
+        sum += number;
+    }
+    let average = sum/listLength;
+    return average;
+}
+
+var list = [2, 6, 8.4, 10, 88, 44];
+console.log(average());
+
+
