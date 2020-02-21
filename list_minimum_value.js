c67ac4b7d83de917d6748bcbd9cd2203772e9994
KPrerana4 <prerana.kannegundla@thoughtworks.com>
2020-02-21 12:24:18 +0530

    minimum value of the list using reduce method

--- list_minimum_value.js
+++ list_minimum_value.js
@@ -13,4 +13,15 @@ function getMinimumValue()
 var list = new Array(2, 10, 8, 30, 1, 100, 88);
 var minimum;
 getMinimumValue();
-console.log(minimum);
+//console.log(minimum);
+
+function main()
+{
+    let list = [2, 10, 8, 30, 1, 100, 88];
+    let minimum = list.reduce(function(accumulator, currentValue){
+            return Math.min(accumulator,currentValue);
+     });
+     console.log("Minimum value of the list : " + minimum);
+}
+
+main();
