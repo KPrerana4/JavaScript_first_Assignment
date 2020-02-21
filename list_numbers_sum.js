699201d0a771dc6bea2547f1ba4b118f63879f7a
KPrerana4 <prerana.kannegundla@thoughtworks.com>
2020-02-21 10:59:19 +0530

    Sum of elements of an array using reduce method

--- list_numbers_sum.js
+++ list_numbers_sum.js
@@ -1,9 +1,21 @@
-list = [1,10.2,20,30.4,2,3,9,44];
+list = [1, 10.2, 20, 30.4, 2, 3, 9, 44];
 var number;
-var sum = 0;
+var sumOfArray = 0;
 for(number of list)
 {
-    sum += number;
+    sumOfArray += number;
 }
-console.log(sum);
 
+function main()
+{
+    let list = [1, 10.2, 20, 30.4, 2, 3, 9, 44];
+    let sumOfList = list.reduce(sum);
+    console.log("Sum of numbers in list : " + sumOfList);
+}
+
+function sum(accumulator, currentValue)
+{
+    return accumulator + currentValue;
+}
+
+main();
