45df13b571ed23cc298a196a561f55336abdf791
KPrerana4 <prerana.kannegundla@thoughtworks.com>
2020-02-21 10:41:34 +0530

    selecting odd numbers using filter method

--- odd_numbers.js
+++ odd_numbers.js
@@ -8,4 +8,11 @@ for(number of numbersList)
         oddNumbers.push(number);
     }
 }
-console.log(oddNumbers);
+
+function main()
+{
+    let list = [1, -3, 5, 2, 4, 6, 7, -8, 99, 100];
+    let oddNumbers = list.filter(element => element % 2 != 0);
+    console.log("Odd numbers in the list : " + oddNumbers);
+}
+main();
