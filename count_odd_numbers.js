66e9c4cc09ff81a563932c0cf61b3f9d5d970920
KPrerana4 <prerana.kannegundla@thoughtworks.com>
2020-02-21 13:06:30 +0530

    to get number of odd numbers in the list

--- count_odd_numbers.js
+++ count_odd_numbers.js
@@ -13,9 +13,16 @@ function countOddNumbers(list)
 
 function main()
 {
-    let list = [0, 3, 4, 8, '11', 7, 9, 10];
+    let list = [0, 3, 4, 8, '11', 7, 9, 10, '91', 99];
     let oddCount = countOddNumbers(list);
     console.log(oddCount);
+}
+
+function main()
+{
+    let list =  [0, 3, 4, 8, '11', 7, 9, 10, '91', 99];
+    let oddNumbers = list.filter(element => element % 2 != 0 );
+    console.log("Number of odd number in the list : " + oddNumbers.length);
 }
 
 main();
