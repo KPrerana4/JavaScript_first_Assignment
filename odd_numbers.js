e87bce75ed6c51b5f5ce7a0ee005e716e69e7126
KPrerana4 <prerana.kannegundla@thoughtworks.com>
2020-02-23 18:33:42 +0530

    using filter method

--- odd_numbers.js
+++ odd_numbers.js
@@ -9,6 +9,19 @@ function findOddNumbers(list)
     return oddNumbers;
 }
 
+function checkOdd(element)
+{
+    return element % 2 != 0;
+}
+
+function findOddNumbers(list)
+{
+    let oddNumbers = [];
+    oddNumbers = list.filter(checkOdd);
+    return oddNumbers;
+}
+
+
 function main()
 {
     let list = [1, 0, -3, "11", 56, 101, 98, "12"];
