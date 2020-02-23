456d46d00d92a92b7d7ff34fbc49552ba7a1973c
KPrerana4 <prerana.kannegundla@thoughtworks.com>
2020-02-23 21:39:43 +0530

    add main method

--- list_maximum_value.js
+++ list_maximum_value.js
@@ -1,26 +1,19 @@
-var getMaximumValue = function(){
-    maximum = list[0];
-    for(let element of list)
-    {
-        if(element > maximum)
-        {
+function maximumOfList(list)
+{
+    let element, maximum = list[0];
+    for(element of list){
+        if(maximum < element){
             maximum = element;
         }
     }
+    return maximum;
 }
-
-var list = new Array(1, 10, 8, 30, 25, 100, 88);
-var maximum;
-getMaximumValue();
-//console.log(maximum);
 
 function main()
 {
-    let list = [1, 10, 8, 30, 25, 100, 88, 120, 160];
-    let maximum = list.reduce(function(accumulator, currentValue){
-            return Math.max(accumulator, currentValue);
-    });
-    console.log("Maximum value of list : " + maximum);
+    let list = [1, 10, 8, 180, 25, 100, 88, 120, 160];
+    let maximum = maximumOfList(list);
+    console.log("Maximum value : " + maximum);
 }
 
 main();
