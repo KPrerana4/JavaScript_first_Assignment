cd0312339b51363892b3109ac4ae4913ae1c0b77
KPrerana4 <prerana.kannegundla@thoughtworks.com>
2020-02-23 23:05:58 +0530

    add another method checkDescending which uses filter

--- checkDescending.js
+++ checkDescending.js
@@ -9,12 +9,23 @@ function checkDescendingOrder(list)
    return order;
 }
 
+function checkDescendingOrder(list)
+{
+    let newList = list.filter(function(element, index, list){
+       return list[index] > list[index + 1];
+    });
+    return newList.length + 1 == list.length;
+}
+
+
 function main()
 {
-    let list = [2,4,1,6,5];
-    console.log(list+" is in descending order:"+checkDescendingOrder(list));
-    list = [40,30,20,10];
-    console.log(list+" is in descending order:"+checkDescendingOrder(list));
+    let list = [2, 4, 1, 6, 5];
+    console.log(list+" is in descending order : "+checkDescendingOrder(list));
+    list = [40, 30, 20, 10];
+    console.log(list+" is in descending order : "+checkDescendingOrder(list));
+    list = [36, 40, 30, 20, 10];
+    console.log(list+" is in descending order : "+checkDescendingOrder(list));
 }
 
 main();
