bab691075fbc534f750da3a6e027fa76ae47b437
KPrerana4 <prerana.kannegundla@thoughtworks.com>
2020-02-23 19:19:47 +0530

    add main method

--- reverse_list.js
+++ reverse_list.js
@@ -1,21 +1,18 @@
-//var list = [1, 'a', "abcd", "b", 3.6, 'wxyz'];
-//reverseList();
-
-function reverseList()
+function reverseTheList(list)
 {
-    let listLength = list.length;
-    for(let index = listLength-1; index >= listLength/2; index--)
-    {
-        let element = list[index];
+    let listLength = list.length, temporaryContainer;
+    for(let index = 0 ; index < listLength/2 ; index++){
+        temporaryContainer = list[index];
         list[index] = list[listLength - index - 1];
-        list[listLength - index - 1] = element;
+        list[listLength - index - 1] = temporaryContainer;
     }
+    return list;
 }
 
 function main()
 {
-    let list = [1, 'a', "abcd", "b", 3.6, 'wxyz'];
-    console.log("Reverse order of the list: " + list.reverse());
+    let list = [1, 'a', "abcd", "b", 3.6, 'wxyz', 98, 120.86];
+    console.log("List in reverse order : " + reverseTheList(list));
 }
 
 main();
