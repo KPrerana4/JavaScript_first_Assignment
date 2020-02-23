967afa0585b54d5a42d899f9c025f0404d3f360b
KPrerana4 <prerana.kannegundla@thoughtworks.com>
2020-02-23 22:13:53 +0530

    add method countOddNumbers

--- count_odd_numbers.js
+++ count_odd_numbers.js
@@ -11,18 +11,17 @@ function countOddNumbers(list)
     return oddCount;
 }
 
+function countOddNumbers(list)
+{
+    let oddNumbers = list.filter(element => element % 2 != 0 );
+    return oddNumbers.length;
+}
+
 function main()
 {
     let list = [0, 3, 4, 8, '11', 7, 9, 10, '91', 99];
     let oddCount = countOddNumbers(list);
     console.log(oddCount);
-}
-
-function main()
-{
-    let list =  [0, 3, 4, 8, '11', 7, 9, 10, '91', 99];
-    let oddNumbers = list.filter(element => element % 2 != 0 );
-    console.log("Number of odd number in the list : " + oddNumbers.length);
 }
 
 main();
