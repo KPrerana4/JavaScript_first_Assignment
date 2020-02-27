d8ab49dac92004521008fd1dd8188f260463e473
KPrerana4 <prerana.kannegundla@thoughtworks.com>
2020-02-27 11:19:18 +0530

    change function

--- even_numbers.js
+++ even_numbers.js
@@ -9,21 +9,20 @@ function findEvenNumbers(list)
     return evenNumbers;
 }
 
+function getEvenNumbers(list)
+{
+    return list.filter(checkEven);
+}
+
 function checkEven(element)
 {
     return element % 2 == 0;
 }
 
-function findEvenNumbers(list)
-{
-    let evenNumbers = list.filter(checkEven);
-    return evenNumbers;
-}
-
 function main()
 {
     let list = [1, 0, -3, "11", 56, 101, 98, "12"];
-    let evenNumbers = findEvenNumbers(list);
+    let evenNumbers = getEvenNumbers(list);
     console.log("Odd numbers in the list : " + evenNumbers);
 }
 
